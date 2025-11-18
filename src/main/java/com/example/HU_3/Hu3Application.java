2e2057e872e0b0501a5d41100c4ea26f01d1ae23
wilffren <wilffren14@gmail.com>
2025-11-18 17:49:16 -0500

    Feat: The structure was created

---
+++ src/main/java/com/example/HU_3/Hu3Application.java
@@ -0,0 +1,13 @@
+package com.example.HU_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hu3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hu3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HU_3/Hu3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HU_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hu3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
