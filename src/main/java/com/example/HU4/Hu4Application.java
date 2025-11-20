d3e454c98423bf993dee2da1e572a0f74b5a87d1
wilffren <wilffren14@gmail.com>
2025-11-20 18:31:15 -0500

    Feat: THe project was created

---
+++ src/main/java/com/example/HU4/Hu4Application.java
@@ -0,0 +1,13 @@
+package com.example.HU4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hu4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hu4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HU4/Hu4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HU4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hu4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
